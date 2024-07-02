15551d63845b71660bc59333a97276a922924cb2
Paulo Marcio Brandi Rezende <paulo@pmbr.net>
2024-07-02 11:42:28 -0400

    tcp socket

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Starting Polenta Go")
+	// Listen for incoming connections
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+		// Accept incoming connections
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection in a goroutine
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// Create a buffer to read data into
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read data from the client
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Process and use the data (here, we'll just print it)
+		fmt.Printf("Received: %s\n", buffer[:n])
+	}
 }
