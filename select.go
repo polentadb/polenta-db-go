e6fb007eeab00632207d3dc5e5c760ffc18e1e07
Paulo Marcio Brandi Rezende <paulo@pmbr.net>
2024-07-10 12:25:55 -0400

    sorter package

--- select.go
+++ select.go
@@ -1,7 +1,10 @@
 package main
 
-import data "polenta/data"
+import (
+	data "polenta/data"
+	sorter "polenta/sorter"
+)
 
 func selectFrom(rows data.Rows, _ string, _ string, order string) data.Rows {
-	return data.Rows(SortableRows(rows).Sort(order))
+	return data.Rows(sorter.SortableRows(rows).Sort(order))
 }
